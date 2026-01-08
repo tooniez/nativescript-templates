0e674de521d337818de10fedade75bd51895b8ce
Nathan Walker <walkerrunpdx@gmail.com>
2026-01-07 18:34:28 -0800

    feat(vue): simplify setup

--- packages/template-blank-vue/app/components/Home.vue
+++ packages/template-blank-vue/app/components/Home.vue
@@ -5,12 +5,7 @@
         </ActionBar>
 
         <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
+            <Label class="text-xl align-middle text-center text-gray-500" :text="message" />
         </GridLayout>
     </Page>
 </template>
@@ -24,18 +19,3 @@
     }
   };
 </script>
-
-<style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
-
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
-</style>
